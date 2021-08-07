e2d8bd6540a7d9d514becfadca4de2aa030de014
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-08-07 09:35:12 +0200

    ad eslint

--- js/site.js
+++ js/site.js
@@ -1,67 +1,67 @@
+/* eslint-disable no-use-before-define */
 // controller function
 function handleFizzBuzzBtnClick() {
-    // get user input
-    const fizzValue = getFizzValue();
-    const buzzValue = getBuzzValue();
+  // get user input
+  const fizzValue = getFizzValue();
+  const buzzValue = getBuzzValue();
 
-    // validate user input
+  // validate user input
 
-    // prepare table rows to display
-    const tableRows = generateTableHtml(fizzValue, buzzValue);
+  // prepare table rows to display
+  const tableRows = generateTableHtml(fizzValue, buzzValue);
 
-    // display data to user
-    displayFizzBuzz(tableRows);
+  // display data to user
+  displayFizzBuzz(tableRows);
 }
 
 // logic functions
 function getFizzValue() {
-    return Number(document.getElementById('fizzValue').value);
+  return Number(document.getElementById('fizzValue').value);
 }
 
 function getBuzzValue() {
-    return Number(document.getElementById('buzzValue').value);
+  return Number(document.getElementById('buzzValue').value);
 }
 
-function generateTableHtml(startingValue, endingValue){
-    const numArray = generateArr(startingValue, endingValue);
+function generateTableHtml(startingValue, endingValue) {
+  const numArray = generateArr(startingValue, endingValue);
 
-    let tableRows = '';
+  let tableRows = '';
 
-    for (const number of numArray) {
-        tableRows += processArrayElement(number);
-    }
+  numArray.forEach((number) => {
+    tableRows += processArrayElement(number);
+  });
 
-    return tableRows;
+  return tableRows;
 }
 
 function processArrayElement(number) {
-    if (number % 3 === 0 && number % 5 === 0) {
-        return `<tr><td>FizzBuzz</td></tr>`
-    }else if(number % 3 === 0) {
-        return `<tr><td>Fizz</td></tr>`
-    }else if(number % 5 === 0){
-        return `<tr><td>Buzz</td></tr>`
-    }else {
-        return `<tr><td>${number}</td></tr>`
-    }
+  if (number % 3 === 0 && number % 5 === 0) {
+    return '<tr><td>FizzBuzz</td></tr>';
+  } if (number % 3 === 0) {
+    return '<tr><td>Fizz</td></tr>';
+  } if (number % 5 === 0) {
+    return '<tr><td>Buzz</td></tr>';
+  }
+  return `<tr><td>${number}</td></tr>`;
 }
 
-function generateArr(startingValue, endingValue){
-    const arr = [];
+function generateArr(startingValue, endingValue) {
+  const arr = [];
 
-    for (let i = startingValue; i <= endingValue; i++) {
-        arr.push(i);
-    }
+  for (let i = startingValue; i <= endingValue; i++) {
+    arr.push(i);
+  }
 
-    return arr;
+  return arr;
 }
 
 // html input is type of 'number' therefore there is no need to validate type
 // the only two validations performed:
 // - check if each input value is in 0 - 100 range
 // check if buzz >= fizz
-function validateFizzValue(fizzValue) {
-    
+function validateFizzValue(fizzValue) { 
+
 }
 
 function validateBuzzValue(buzzValue) {
@@ -74,5 +74,5 @@ function isBuzzGreaterThanFizz() {
 
 // view functions
 function displayFizzBuzz(tableRows) {
-    document.getElementById('results').innerHTML = tableRows;
+  document.getElementById('results').innerHTML = tableRows;
 }
